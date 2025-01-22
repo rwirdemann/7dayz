89a7e1499ace2c1d7c11d809a0d94706002cbbdc
Ralf Wirdemann <ralf.wirdemann@mailbox.org>
2025-01-22 21:01:51 +0100

    Update box functionality and add item movement support
    Refactor to track and render the active box, ensuring the selection pointer is limited to it. Introduce terminal dimension handling and new keyboard shortcuts for moving items between boxes. Simplify the `NewBox` function by removing width and height parameters.

--- box.go
+++ box.go
@@ -10,13 +10,19 @@ import (
 	"strings"
 )
 
+// ActiveBox represents the currently active box, identified by its title. The selection pointer is only rendered for
+// the currently active box.
+var ActiveBox = "Inbox"
+
 type Item string
 
 func (i Item) FilterValue() string {
 	return ""
 }
 
-type itemDelegate struct{}
+type itemDelegate struct {
+	boxTitle string
+}
 
 func (d itemDelegate) Height() int                             { return 1 }
 func (d itemDelegate) Spacing() int                            { return 0 }
@@ -28,7 +34,9 @@ func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list
 	}
 
 	fn := lipgloss.NewStyle().PaddingLeft(2).Render
-	if index == m.Index() {
+
+	// Render selection pointer only for the currently active box.
+	if ActiveBox == d.boxTitle && index == m.Index() {
 		fn = func(s ...string) string {
 			return lipgloss.NewStyle().PaddingLeft(0).Foreground(lipgloss.Color("170")).Render("> " + strings.Join(s, " "))
 		}
@@ -44,8 +52,8 @@ type Box struct {
 	list.Model
 }
 
-func NewBox(title string, items []list.Item, width, height int) Box {
-	l := list.New(items, itemDelegate{}, width, height)
+func NewBox(title string, items []list.Item) Box {
+	l := list.New(items, itemDelegate{boxTitle: title}, 0, 0)
 	l.Title = title
 	l.SetShowTitle(false)
 	l.SetShowStatusBar(false)
