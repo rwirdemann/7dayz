0156f7db7cb685f4c205aaf74ba42e40aa4d64a1
ralfwirdemann <ralf.wirdemann@codekeepers.de>
2025-01-26 15:56:28 +0100

    Add shortcut handling and refactor tab model logic
    Introduce centralized shortcut handling with predefined groups for better key management. Refactor tab model logic to simplify task initialization, tab switching, and task operations, improving maintainability and readability. Additionally, update help view to dynamically display shortcuts.

---
+++ shortcuts.go
@@ -0,0 +1,38 @@
+package _dayz
+
+var (
+	KeyNextPanel = "tab"
+	KeyPrefPanel = "shift+tab"
+)
+
+type Shortcut struct {
+	Key  string
+	Desc string
+}
+
+var General = []Shortcut{
+	{KeyNextPanel, "focus next day"},
+	{KeyPrefPanel, "focus prev day"},
+	{"alt+0", "focus inbox"},
+	{"alt+{i}", "focus weekday"},
+	{"?", "toggle help"},
+}
+
+var Management = []Shortcut{
+	{"backspace", "delete task"},
+	{"space", "complete task"},
+	{"enter", "edit task"},
+	{"n", "new task"},
+}
+
+var Movement = []Shortcut{
+	{"shift+right", "move task right"},
+	{"shift+left", "move task left"},
+	{"shift+t", "move task to today"},
+	{"shift+i", "move task to inbox"},
+}
+
+var Sorting = []Shortcut{
+	{"shift+up", "move task up"},
+	{"shift+down", "move task down"},
+}
